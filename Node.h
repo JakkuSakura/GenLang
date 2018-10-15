ff05d45cc15bcecda19696f40a1d4386f3b445f1
QiuJiangkun <qiujiangkun@foxmail.com>
2018-10-15 12:39:08 +0800

    flush files

--- Node.h
+++ Node.h
@@ -1,40 +1,128 @@
-//
-// Created by Lenovo on 2018/9/26.
-//
+#include <iostream>
+#include <vector>
 
-#ifndef PROJECT_NODE_H
-#define PROJECT_NODE_H
-
-#include <stdlib.h>
-#include <memory.h>
-
-typedef struct __node
+class CodeGenContext;
+class NStatement;
+class NExpression;
+class NVariableDeclaration;
+typedef std::vector<NStatement*> StatementList;
+typedef std::vector<NExpression*> ExpressionList;
+typedef std::vector<NVariableDeclaration*> VariableList;
+class Node
 {
-    int type;
-    union {
-        union {
-            double F64;
-            long long I64;
-            char *STR;
-        };
-        struct {
-            int token;
-            int len;
-            struct __node **childs;
-        };
-    };
-}Node;
-typedef Node *pNode;
-#define YYSTYPE pNode
+public:
+        virtual ~Node() {}
 
-#include "y.tab.h"
+};
 
-#define NODE_MAXN 50000
+class NExpression : public Node
+{
+
+};
 
 
-pNode *new_pNode_array(int i);
-pNode new_node(int flag);
-void add_child_node(pNode r, pNode nd);
-pNode new_node_operation(int tk);
-void free_nodes();
-#endif //PROJECT_NODE_H
+
+class NStatement : public Node
+{
+
+};
+
+
+
+class NInteger : public NExpression
+{
+public:
+        long long value;
+        NInteger(long long value) : value(value) { }
+
+};
+
+class NDouble : public NExpression
+{
+public:
+        double value;
+        NDouble(double value) : value(value) { }
+
+};
+
+class NIdentifier : public NExpression
+{
+public:
+        std::string name;
+        NIdentifier(const std::string& name) : name(name) { }
+
+};
+
+class NMethodCall : public NExpression
+{
+public:
+        const NIdentifier& id;
+        ExpressionList arguments;
+        NMethodCall(const NIdentifier& id, ExpressionList& arguments) :
+                id(id), arguments(arguments) { }
+        NMethodCall(const NIdentifier& id) : id(id) { }
+
+};
+class NBinaryOperator : public NExpression
+{
+public:
+        int op;
+        NExpression& lhs;
+        NExpression& rhs;
+        NBinaryOperator(NExpression& lhs, int op, NExpression& rhs) :
+                lhs(lhs), rhs(rhs), op(op) { }
+
+};
+
+class NAssignment : public NExpression
+{
+public:
+        NIdentifier& lhs;
+        NExpression& rhs;
+        NAssignment(NIdentifier& lhs, NExpression& rhs) :
+                lhs(lhs), rhs(rhs) { }
+
+};
+
+class NBlock : public NExpression
+{
+public:
+        StatementList statements;
+        NBlock() { }
+
+};
+
+class NExpressionStatement : public NStatement
+{
+public:
+        NExpression& expression;
+        NExpressionStatement(NExpression& expression) :
+                expression(expression) { }
+
+};
+
+class NVariableDeclaration : public NStatement
+{
+public:
+        const NIdentifier& type;
+        NIdentifier& id;
+        NExpression *assignmentExpr;
+        NVariableDeclaration(const NIdentifier& type, NIdentifier& id) :
+                type(type), id(id) { }
+        NVariableDeclaration(const NIdentifier& type, NIdentifier& id, NExpression *assignmentExpr) :
+                type(type), id(id), assignmentExpr(assignmentExpr) { }
+
+};
+
+class NFunctionDeclaration : public NStatement
+{
+public:
+        const NIdentifier& type;
+        const NIdentifier& id;
+        VariableList arguments;
+        NBlock& block;
+        NFunctionDeclaration(const NIdentifier& type, const NIdentifier& id,
+                        const VariableList& arguments, NBlock& block) :
+                type(type), id(id), arguments(arguments), block(block) { }
+};
+
---
+++ cxx_api.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void readInt(int val)
+{
+	cout << val << endl;
+}
+void readDouble(double val)
+{
+	cout << val << endl;
+}
+void readLong(long long val)
+{
+	cout << val << endl;
+}
+void readFloat(float val)
+{
+	cout << val << endl;	
+
+}
+void read
