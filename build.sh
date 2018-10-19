01baf24a1b27d22bdcdfbbd1527394d3c56df786
QiuJiangkun <qiujiangkun@foxmail.com>
2018-10-19 14:17:17 +0800

    changed

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 bison -d -o src/genlang.parser.cc yacc/genlang.y
 flex -o src/genlang.scanner.cc lex/genlang.l
+mv src/genlang.parser.hh includes/genlang.parser.hh
 rm genlang testlex
 g++ src/*.cc src/main/*.cpp -o genlang -Iincludes -g
 # g++ src/*.cpp test/lex.cpp -o testlex -Iincludes
